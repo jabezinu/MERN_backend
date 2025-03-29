52cec41f4314cd9097fec1307a46f079423e3aee
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-03-29 04:21:46 +0300

    done with the current user profile

--- my-contacts/routes/user.route.js
+++ my-contacts/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { registerUser, loginUser, currentUser } = require("../controllers/user.controller");
+const validateToken = require("../middleware/validateTokenHandler");
 
 const userRouter = express.Router();
 
 userRouter.route("/register").post(registerUser)
 userRouter.route("/login").post(loginUser)
-userRouter.route("/current").get(currentUser)
+userRouter.route("/current").get(validateToken, currentUser)
 
 module.exports = userRouter
