169ebaec1e712cf077552e55a5f1bc52308ed45c
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-03-29 04:20:41 +0300

    done with the current user profile

--- my-contacts/controllers/user.controller.js
+++ my-contacts/controllers/user.controller.js
@@ -65,7 +65,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route GET /api/users/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-    
+    res.json(req.user);
 })
 
 module.exports = {registerUser, loginUser, currentUser}
