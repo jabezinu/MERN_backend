3831d1a341888895adc1f4c0683bab74bf59504d
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-01 09:39:09 +0300

    add controllers

--- my-contacts/controllers/contact.controller.js
+++ my-contacts/controllers/contact.controller.js
@@ -1,20 +1,19 @@
 const asyncHandler = require("express-async-handler");
 const Contact = require("../models/contacts.model");
 const mongoose = require("mongoose");
+const userModel = require("../models/user.model");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//access public
+//access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contact = await Contact.find({});
+    const contact = await Contact.find({user_id: req.user.id});
 
-    if(contact){
-        res.status(200).json(contact)
-    }
+    res.status(200).json(contact)
 })
-//@desc create contact
+//@desc create contact 
 //@route POST /api/contacts
-//access public
+//access private
 const createContact = asyncHandler(async (req, res) => {
     const {name, email, phone} = req.body;
 
@@ -23,13 +22,14 @@ const createContact = asyncHandler(async (req, res) => {
         throw new Error("fill all fields");
     }
     
-    const filterPhone = await Contact.findOne({phone})
+    const filterPhone = await Contact.findOne({phone, user_id: req.user.id})
     if(filterPhone){
         res.status(400);
         throw new Error("already exits");
     }
     
     const contact = await Contact.create({
+        user_id: req.user.id,
         name,
         email,
         phone
@@ -41,7 +41,7 @@ const createContact = asyncHandler(async (req, res) => {
 })
 //@desc Get contact
 //@route GET /api/contacts/:id
-//access public
+//access private
 const getContact = asyncHandler (async (req, res) => {
     const {id} = req.params;
     
@@ -49,41 +49,48 @@ const getContact = asyncHandler (async (req, res) => {
         res.status(400);
         throw new Error("no such contact");
     }
-    const contact = await Contact.findById(id);
 
-    if (!contact) {
-        res.status(404);
-        throw new Error("no such contact")
+    const contact = await Contact.findById(id);    
+    if (contact.user_id == req.user.id) {
+        res.status(200).json(contact)
+    }else{
+        res.status(403);
+        throw new Error("you are not authrized to access this contact.")
     }
-
-    res.status(200).json(contact)
-
+    
+    
 })
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//access public
+//access private
 const updateContact = asyncHandler(async (req, res) => {
     const {id} = req.params;
     const {phone} = req.body; // Extract phone from req.body
-
+    
     if(!mongoose.Types.ObjectId.isValid(id)){
         res.status(401);
         throw new Error("no such contact");
     }
     
     const filterPhone = await Contact.findOne({phone});
-    if(filterPhone){
+    if(filterPhone.user_id == req.user.id){
         res.status(400);
         throw new Error("already exists");
     }
-
-
-    const updatedContact = await Contact.findByIdAndUpdate(
-        id,
-        req.body,
-        {new: true}
-    );
+    
+    let updatedContact;
+    const contact = await Contact.findById(id);    
+    if (contact.user_id == req.user.id){
+        updatedContact = await Contact.findByIdAndUpdate(
+            id,
+            req.body,
+            {new: true}
+        );
+    }else{
+        res.status(403);
+        throw new Error("you are not authrized to access this contact.")
+    }
     if(!updatedContact) {
         res.status(404);
         throw new Error("Contact not found");
@@ -93,7 +100,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 //@desc Get all contact
 //@route GET /api/contacts
-//access public
+//access private
 const deleteContact = asyncHandler(async (req, res) => {
     const {id} = req.params;
     
--- my-contacts/routes/contact.route.js
+++ my-contacts/routes/contact.route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const { getContacts, createContact, getContact, updateContact, deleteContact } = require("../controllers/contact.controller");
+const validateToken = require("../middleware/validateTokenHandler");
 
 const contactRouter = express.Router();
 
-contactRouter.route("/").get(getContacts)
-contactRouter.route("/").post(createContact)
-contactRouter.route("/:id").get(getContact)
-contactRouter.route("/:id").put(updateContact)
-contactRouter.route("/:id").delete(deleteContact)
+contactRouter.use(validateToken)
+contactRouter.route("/").get(getContacts).post(createContact)
+contactRouter.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 
 module.exports = contactRouter
