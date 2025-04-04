2496d349fbef4b96c3c2913e06f3b7fb327eb911
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-04-04 05:10:14 +0300

    add login route and controller

--- prescripto_2/backend/controllers/adminController.js
+++ prescripto_2/backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 // API for adding doctor
 const addDoctor = async (req, res) => {
@@ -23,7 +24,7 @@ const addDoctor = async (req, res) => {
         if(password.length < 8){
             return res.status(400).json({sucess: false, message: "please enter Stron password."})
         }
-
+        
         // hashing Doctor's password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt) 
@@ -31,7 +32,7 @@ const addDoctor = async (req, res) => {
         // upload image to cloudinary
         const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"})
         const imageUrl = imageUpload.secure_url
-
+        
         // save into the DB
         const doctorData = {
             name,
@@ -58,4 +59,26 @@ const addDoctor = async (req, res) => {
     }
 }
 
-export{addDoctor}
+// API For admin Login
+const loginAdmin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if(email == process.env.ADMIN_EMAIL && password == process.env.ADMIN_PASSWORD ){
+            
+            const token = await jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success: true ,token})
+            
+        }else{
+            return res.json({sucess: false, message: "Invalid credentials"})
+            
+        }
+
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})        
+    }
+}
+
+export{addDoctor, loginAdmin}
